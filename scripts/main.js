6d814f8a3318822ae1857f1e9f63d14340ddc49f
Slyfti <titout24@outlook.fr>
2026-01-28 18:18:14 +0100

    rework: extension loads fully after spotify UI init

--- scripts/main.js
+++ scripts/main.js
@@ -7,99 +7,101 @@
 
 eraseButton();
 loadChecker();
+run();
 console.log("Translatify: Lyrics Translator is running..");
 
-// First install cases
-chrome.storage.local.get(['translateButton'], (result) => {
-    if (result.translateButton == null) {
-        chrome.storage.local.set({translateButton: true}, () => {
-            toggleTranslateButton();
-            console.log("Translatify: Translate button is enabled by default");
-        });
+function run() {
+    // First install cases
+    chrome.storage.local.get(['translateButton'], (result) => {
+        if (result.translateButton == null) {
+            chrome.storage.local.set({ translateButton: true }, () => {
+                toggleTranslateButton();
+                console.log("Translatify: Translate button is enabled by default");
+            });
+        }
     }
-}
-);
-chrome.storage.local.get(['language'], (result) => {
-    let defaultLanguage = navigator.language;
-    if (result.language == null) {
-        chrome.storage.local.set({language: defaultLanguage}, () => {
-            console.log("Translatify: Language is set to default");
+    );
+    chrome.storage.local.get(['language'], (result) => {
+        let defaultLanguage = navigator.language;
+        if (result.language == null) {
+            chrome.storage.local.set({ language: defaultLanguage }, () => {
+                console.log("Translatify: Language is set to default");
+                restoreLyrics();
+                translate();
+            });
+        }
+    }
+    );
+
+    // gets the option values from the extension storage
+    chrome.storage.local.get(['lyricsMode'], (result) => {
+
+        // Apply the option values to the page
+        if (result.lyricsMode) {
+            if (result.lyricsMode == "replace") {
+                document.documentElement.style.setProperty('--newLyricsSize', '1em');
+                document.documentElement.style.setProperty("--lyricsDisplay", "none");
+                document.documentElement.style.setProperty('--newLyricsOpacity', '1');
+                document.documentElement.style.setProperty("--newLyricsLineHeight", "1.5em");
+
+            } else if (result.lyricsMode == "along") {
+                document.documentElement.style.setProperty('--newLyricsOpacity', '0.5');
+                document.documentElement.style.setProperty('--newLyricsSize', '0.75em');
+                document.documentElement.style.setProperty("--lyricsDisplay", "block");
+                document.documentElement.style.setProperty("--newLyricsLineHeight", "1em");
+
+            }
+            console.log("Translatify: Lyrics size updated!");
+
+        }
+    }
+    );
+
+
+    chrome.storage.local.get(['newLyricsSize'], (result) => {
+        if (result.newLyricsSize) {
+            document.documentElement.style.setProperty('--newLyricsSize', result.newLyricsSize + 'em');
+            console.log("Translatify: Lyrics size updated!");
+        }
+    });
+
+
+    chrome.runtime.onMessage.addListener(msgObj => {
+        if (msgObj.updateLanguage) {
+            console.log("Translatify: Language updated!");
             restoreLyrics();
             translate();
-        });
-    }
-}
-);
+        }
 
+        if (msgObj.toggleTranslation !== undefined) {
+            console.log("Translatify: Translation toggle updated via popup");
+        }
 
-
-// gets the option values from the extension storage
-chrome.storage.local.get(['lyricsMode'], (result) => {
-
-    // Apply the option values to the page
-    if (result.lyricsMode) {
-        if (result.lyricsMode == "replace") {
-            document.documentElement.style.setProperty('--newLyricsSize', '1em');
-            document.documentElement.style.setProperty("--lyricsDisplay", "none");
-            document.documentElement.style.setProperty('--newLyricsOpacity', '1');
-            document.documentElement.style.setProperty("--newLyricsLineHeight", "1.5em");
-
-        } else if (result.lyricsMode == "along") {
-            document.documentElement.style.setProperty('--newLyricsOpacity', '0.5');
-            document.documentElement.style.setProperty('--newLyricsSize', '0.75em');
-            document.documentElement.style.setProperty("--lyricsDisplay", "block");
-            document.documentElement.style.setProperty("--newLyricsLineHeight", "1em");
+        if (msgObj.newLyricsSize) {
+            document.documentElement.style.setProperty('--newLyricsSize', msgObj.newLyricsSize + 'em');
+            console.log("Translatify: Lyrics size updated!");
 
         }
-        console.log("Translatify: Lyrics size updated!");
-        
-    }
-}
-);
+
+        if (msgObj.lyricsMode) {
+
+            if (msgObj.lyricsMode == "replace") {
+                document.documentElement.style.setProperty('--newLyricsSize', '1em');
+                document.documentElement.style.setProperty("--lyricsDisplay", "none");
+                document.documentElement.style.setProperty('--newLyricsOpacity', '1');
+                document.documentElement.style.setProperty("--newLyricsLineHeight", "1.5em");
+
+            } else if (msgObj.lyricsMode == "along") {
+                document.documentElement.style.setProperty('--newLyricsOpacity', '0.5');
+                document.documentElement.style.setProperty('--newLyricsSize', '0.75em');
+                document.documentElement.style.setProperty("--lyricsDisplay", "block");
+                document.documentElement.style.setProperty("--newLyricsLineHeight", "1em");
 
 
-chrome.storage.local.get(['newLyricsSize'], (result) => {
-    if (result.newLyricsSize) {
-        document.documentElement.style.setProperty('--newLyricsSize', result.newLyricsSize + 'em');
-        console.log("Translatify: Lyrics size updated!");
-    }
-});
-
-
-chrome.runtime.onMessage.addListener(msgObj => {
-    if (msgObj.updateLanguage) {
-        console.log("Translatify: Language updated!");
-        restoreLyrics();
-        translate();
-    }
-
-    if (msgObj.toggleTranslation !== undefined) {
-        console.log("Translatify: Translation toggle updated via popup");
-    }
-
-    if (msgObj.newLyricsSize) {
-        document.documentElement.style.setProperty('--newLyricsSize', msgObj.newLyricsSize + 'em');
-        console.log("Translatify: Lyrics size updated!");
-        
-    }
-
-    if (msgObj.lyricsMode) {
-
-        if (msgObj.lyricsMode == "replace") {
-            document.documentElement.style.setProperty('--newLyricsSize', '1em');
-            document.documentElement.style.setProperty("--lyricsDisplay", "none");
-            document.documentElement.style.setProperty('--newLyricsOpacity', '1');
-            document.documentElement.style.setProperty("--newLyricsLineHeight", "1.5em");
-
-        } else if (msgObj.lyricsMode == "along") {
-            document.documentElement.style.setProperty('--newLyricsOpacity', '0.5');
-            document.documentElement.style.setProperty('--newLyricsSize', '0.75em');
-            document.documentElement.style.setProperty("--lyricsDisplay", "block");
-            document.documentElement.style.setProperty("--newLyricsLineHeight", "1em");
-
+            }
+            console.log("Translatify: Lyrics size updated!");
 
         }
-        console.log("Translatify: Lyrics size updated!");
-        
-    }
-});
+    });
+}
+
